f6dcaeb6d8a02a01e7cccded3eb7a7c91da1d967
Yuanyi Xue <xyy1122@gmail.com>
2015-03-01 00:39:07 -0500

    Bug fix
    Dangerously memory hog....

--- scene_detect-clustering/SceneCutSegmentation.m
+++ scene_detect-clustering/SceneCutSegmentation.m
@@ -5,6 +5,7 @@
 % This script uses Tucker decomposition to find a compact set of basis for
 % clustering the scene. It requires the Sandia Tensor Toolbox and scPos.mat
 
+clear all
 load scPos.mat;
 
 VidPath = '..\videos\mlbpb_23570674_600K.mp4';
@@ -21,25 +22,26 @@ trimN = 3;
 
 % Get the segments
 numSeg = 150; % number of segments
-randIdx = randperm(length(scPos));
+randIdx = randperm(length(scPos)-1);
 segIdx = [scPos(randIdx(1:numSeg))+trimN scPos(randIdx(1:numSeg)+1)-trimN]; % trim a bit
 
 % 4-D tensor for storing all data
-readySegGray = tenzeros(ROIspatial(2),ROIspatial(4),temporalCut,numSeg);
+readySegGray = tenzeros(round(ROIspatial(2)/2),round(ROIspatial(4)/2),temporalCut,numSeg);
 % Query the video and store the data
 for idx=1:numSeg,
     currSeg = double(read(VidObj,segIdx(idx,:)));
     currSegGray = squeeze(0.2989*currSeg(:,:,1,:) ...
         + 0.5870*currSeg(:,:,2,:) + 0.1140*currSeg(:,:,3,:));
     % Only consider the ROI region
-    currSegGray = currSegGray(ROIspatial(1):ROIsptial(2),...
+    currSegGray = currSegGray(ROIspatial(1):ROIspatial(2),...
         ROIspatial(3):ROIspatial(4),:);
+    currSegGray = imresize(currSegGray,0.5,'nearest');
     for i = 1:size(currSegGray,3),
         currSegGray(:,:,i) = currSegGray(:,:,i)-mean2(squeeze(currSegGray(:,:,i)));
     end
     % Temporally resize to 200 frames
     if size(currSegGray,3)>=temporalCut,
-        allIdx = randperm(size(currSegGray));
+        allIdx = randperm(size(currSegGray,3));
         keepIdx = sort(allIdx(1:temporalCut),'ascend');
         readySegGray(:,:,:,idx) = currSegGray(:,:,keepIdx);
     else
