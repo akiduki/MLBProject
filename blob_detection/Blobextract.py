fc99f1138359a0e81fabec29a93ad4cedbae181b
dawnknight <dawnandyknight@gmail.com>
2015-03-06 15:22:26 -0500

    update

--- blob_detection/Blobextract.py
+++ blob_detection/Blobextract.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-def BlobExtract(vidname,ith=30,pth=0,skip = 0,fnum = 500 ,nbuff = 241,fac=8):
+def BlobExtract(vidname,ith=30,pth=0,skip = 0,fnum = 500 ,nbuff = 241,fac=1):
 
     #ith : intensity differece 
     #pth : threshold for number of pixels in certain label
     #skip: skip first N frame
     #maskpath : 0 :no mask , others : mask path 
-    #fnum : how many frame you want to process
+    #fnum : how many frame you want to process # -1 will run till end
     #nbuff : buffer size
     #fac : downsampling scale
 
